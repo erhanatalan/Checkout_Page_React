efbba7572edc1f19d5dfcceb85015d69fece08fe
erhanatalan <erhanatalan2013@gmail.com>
2023-03-04 14:31:56 +0300

    product form

--- src/pages/NewProduct.jsx
+++ src/pages/NewProduct.jsx
@@ -1,14 +1,36 @@
+import axios from "axios";
+import { useState } from "react";
+import ProductForm from "../components/ProductForm";
 
 
-
+const initialState={
+  name:"",
+  image:"",
+  price:0,
+  dampingRate:0.8,
+  amount:1,
+}
 const NewProduct = () => {
-  
+  const url = process.env.REACT_APP_API_URL;
+  const [formData, setFormData] = useState(initialState)
+  const handleChange = (e)=>{
+    setFormData({...formData, [e.terget.id]:e.target.value})
+  }
+  const handleSubmit= async(e)=>{
+    e.preventDefault();
+    try{
+      await axios.post(url, formData)
+      setFormData(initialState);
+    }catch(error){
+      console.log(error);
+    }
 
-  return (
+  }
+  return(
     <div className='container'>
-      
+      <ProductForm handleChange={handleChange} handleSubmit={handleSubmit} formData={formData}/>
     </div>
-  );
+  )
 }
 
 export default NewProduct
